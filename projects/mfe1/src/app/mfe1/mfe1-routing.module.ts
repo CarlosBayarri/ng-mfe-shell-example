7c6a3ef01a0c6da9a257b8fc4f62bf587293eb7b
Carlos Bayarri Cebrecos <e_cbayar@mercadona.com>
2021-09-02 19:25:39 +0200

    changes

--- projects/mfe1/src/app/mfe1/mfe1-routing.module.ts
+++ projects/mfe1/src/app/mfe1/mfe1-routing.module.ts
@@ -3,11 +3,27 @@ import { RouterModule, Routes } from '@angular/router';
 import { Mfe1Component } from './mfe1.component';
 
 const routes: Routes = [
-  { component: Mfe1Component, path: '' },
-  {
-    path: 'products',
-    loadChildren: () => import('../products/products.module').then(m => m.ProductsModule)
-  }
+  { component: Mfe1Component, path: '', children: [
+    {
+      path: '',
+      redirectTo: 'home',
+      pathMatch: 'full'
+    },
+    {
+      path: 'products',
+      loadChildren: () => import('./products/products.module').then(m => m.ProductsModule)
+    },
+    {
+      path: 'home',
+      loadChildren: () => import('./home/home.module').then(m => m.HomeModule)
+    },
+    {
+      path: 'about',
+      loadChildren: () => import('./about/about.module').then(m => m.AboutModule)
+    }
+  ]
+ },
+
 ];
 
 @NgModule({
